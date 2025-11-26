ba40a07b58edb49b663795dd2df575ae774ac97c
CatMolnia <vlad-biryukov-95@mail.ru>
2025-11-26 21:47:30 +0300

    пушу в гитхаб

--- __config.py
+++ __config.py
@@ -1,8 +1,21 @@
+from dataclasses import dataclass, field
+from typing import Dict, Tuple
+
+@dataclass(frozen=True)
 class AppWindowConfig:
-    MAIN_WINDOW_SIZE = (1024, 768)
+    sizes: Dict[str, Tuple[int, int]] = field(
+        default_factory=lambda: {
+            "default": (1024, 768)
+        }
+    )
+    
+    default_size: str = "default"
 
-#class AppColorsConfig:
-
-
-def get_window_config() -> AppWindowConfig:
-    return AppWindowConfig()
+class GeneralInformation:
+    label_title = (
+        "color: #2E2F33; "
+        "background-color: #F1F2F3; "
+        "border-radius: 6px; "
+        "padding: 4px 10px; "
+        "font-size: 10px;"
+    )
--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
 
 from PyQt6.QtWidgets import QApplication, QWidget
-from PyQt6.QtCore import Qt
 
 from cash import Ui_CASH
-from __config import AppWindowConfig, get_window_config
+from __config import AppWindowConfig, GeneralInformation
 
 class Cash(QWidget):
     def __init__(self):
@@ -12,18 +11,27 @@ class Cash(QWidget):
         self.ui = Ui_CASH()
         self.ui.setupUi(self)
 
-        self.config = get_window_config() # get_window_config - функция для получения конфигурации
-        self.start_window = StartWindow(self.config) # StartWindow - класс для создания окна начала работы
-        self.start_window.apply(self) # apply - метод для применения настроек к текущему окну
+        self.config = AppWindowConfig() # config - конфигурация окна
+        self.general_information = GeneralInformation() # general_information - конфигурация для general_information
+
+        self.start_window = StartWindow(self.config, self.general_information) # StartWindow - класс для создания окна начала работы
+        self.start_window.apply(self) # apply - метод для применения настроек к окну
 
 class StartWindow:
-    def __init__(self, config: AppWindowConfig):
+    def __init__(self, config: AppWindowConfig, general_information: GeneralInformation):
         self.config = config
+        self.general_information = general_information
 
     def apply(self, widget: QWidget):
-        width, height = self.config.MAIN_WINDOW_SIZE
+        # размер окна
+        sizes = self.config.sizes
+        size_key = self.config.default_size
+        width, height = sizes.get(size_key, next(iter(sizes.values())))
         widget.resize(width, height)
 
+        # general_information
+        widget.ui.label_title.setStyleSheet(self.general_information.label_title)
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = Cash()
